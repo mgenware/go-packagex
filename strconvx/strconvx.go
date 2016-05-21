4df62cb99d745a1b8993210a7ef621144d78d997
Mgen <m@mgenware.com>
2016-05-22 00:13:51 +0800

    Comments should end with a period

--- strconvx/strconvx.go
+++ strconvx/strconvx.go
@@ -2,12 +2,12 @@ package strconvx
 
 import "strconv"
 
-// ParseFloat returns strconv.ParseFloat(s, 64)
+// ParseFloat returns strconv.ParseFloat(s, 64).
 func ParseFloat(s string) (float64, error) {
 	return strconv.ParseFloat(s, 64)
 }
 
-// ParseFloatOrPanic panics if ParseFloat returns an error
+// ParseFloatOrPanic panics if ParseFloat returns an error.
 func ParseFloatOrPanic(s string) float64 {
 	r, err := ParseFloat(s)
 	if err != nil {
@@ -16,12 +16,12 @@ func ParseFloatOrPanic(s string) float64 {
 	return r
 }
 
-// ParseInt returns strconv.Atoi(s)
+// ParseInt returns strconv.Atoi(s).
 func ParseInt(s string) (int, error) {
 	return strconv.Atoi(s)
 }
 
-// ParseIntOrPanic panics if ParseInt returns an error
+// ParseIntOrPanic panics if ParseInt returns an error.
 func ParseIntOrPanic(s string) (int, error) {
 	r, err := ParseInt(s)
 	if err != nil {
@@ -30,7 +30,7 @@ func ParseIntOrPanic(s string) (int, error) {
 	return r, nil
 }
 
-// ParseUint returns strconv.ParseUint(s, 10, 0)
+// ParseUint returns strconv.ParseUint(s, 10, 0).
 func ParseUint(s string) (uint, error) {
 	r, err := strconv.ParseUint(s, 10, 0)
 	if err != nil {
@@ -39,7 +39,7 @@ func ParseUint(s string) (uint, error) {
 	return uint(r), nil
 }
 
-// ParseUintOrPanic panics if ParseUint returns an error
+// ParseUintOrPanic panics if ParseUint returns an error.
 func ParseUintOrPanic(s string) uint {
 	r, err := ParseUint(s)
 	if err != nil {
