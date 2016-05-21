8d6d1f7ad77206c505e7b2e2e1f2d92bff43d430
Mgen <m@mgenware.com>
2016-05-22 00:29:36 +0800

    mathx: update tests

--- mathx/mathx_test.go
+++ mathx/mathx_test.go
@@ -2,62 +2,78 @@ package mathx
 
 import "testing"
 
-func TestMaxInt(t *testing.T) {
+func errorf(t *testing.T, expected interface{}, got interface{}) {
+	t.Errorf("Expected %v, got %v", expected, got)
+}
+
+func TestMaxMinInt(t *testing.T) {
 	var x, y int
 	x = -1
 	y = 1
 	if r := MaxInt(x, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
+		errorf(t, y, r)
 	}
-	if r := MaxInt(y, x); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
+	if r := MinInt(x, y); r != x {
+		errorf(t, x, r)
 	}
-	if r := MaxInt(y, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
+	if r := MaxInt(x, x); r != x {
+		errorf(t, x, r)
+	}
+	if r := MinInt(x, x); r != x {
+		errorf(t, x, r)
 	}
 }
 
-func TestMaxInt64(t *testing.T) {
+func TestMaxMinInt64(t *testing.T) {
 	var x, y int64
 	x = -1
 	y = 1
 	if r := MaxInt64(x, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
+		errorf(t, y, r)
 	}
-	if r := MaxInt64(y, x); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
-	}
-	if r := MaxInt64(y, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
-	}
-}
-
-func TestMinInt(t *testing.T) {
-	var x, y int
-	x = -1
-	y = 1
-	if r := MinInt(x, y); r != x {
-		t.Errorf("Expected %v, got %v", x, r)
-	}
-	if r := MinInt(y, x); r != x {
-		t.Errorf("Expected %v, got %v", x, r)
-	}
-	if r := MinInt(y, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
-	}
-}
-
-func TestMinInt64(t *testing.T) {
-	var x, y int64
-	x = -1
-	y = 1
 	if r := MinInt64(x, y); r != x {
-		t.Errorf("Expected %v, got %v", x, r)
+		errorf(t, x, r)
 	}
-	if r := MinInt64(y, x); r != x {
-		t.Errorf("Expected %v, got %v", x, r)
+	if r := MaxInt64(x, x); r != x {
+		errorf(t, x, r)
 	}
-	if r := MinInt64(y, y); r != y {
-		t.Errorf("Expected %v, got %v", y, r)
+	if r := MinInt64(x, x); r != x {
+		errorf(t, x, r)
+	}
+}
+
+func TestMaxMinUint(t *testing.T) {
+	var x, y uint
+	x = 9
+	y = 7970
+	if r := MaxUint(x, y); r != y {
+		errorf(t, y, r)
+	}
+	if r := MinUint(x, y); r != x {
+		errorf(t, x, r)
+	}
+	if r := MaxUint(x, x); r != x {
+		errorf(t, x, r)
+	}
+	if r := MinUint(x, x); r != x {
+		errorf(t, x, r)
+	}
+}
+
+func TestMaxMinUint64(t *testing.T) {
+	var x, y uint64
+	x = 9
+	y = 7970
+	if r := MaxUint64(x, y); r != y {
+		errorf(t, y, r)
+	}
+	if r := MinUint64(x, y); r != x {
+		errorf(t, x, r)
+	}
+	if r := MaxUint64(x, x); r != x {
+		errorf(t, x, r)
+	}
+	if r := MinUint64(x, x); r != x {
+		errorf(t, x, r)
 	}
 }
