6d613c1fd4ab7c4ee5e1936bfe0393d6ce7218e6
Mgen <m@mgenware.com>
2016-05-23 23:48:56 +0800

    Remove ParseXXXOrPanic funcs

--- strconvx/parse.go
+++ strconvx/parse.go
@@ -7,27 +7,9 @@ func ParseFloat(s string) (float64, error) {
 	return strconv.ParseFloat(s, 64)
 }
 
-// ParseFloatOrPanic panics if ParseFloat returns an error.
-func ParseFloatOrPanic(s string) float64 {
-	r, err := ParseFloat(s)
-	if err != nil {
-		panic(err)
-	}
-	return r
-}
-
 // ParseInt returns strconv.Atoi(s).
 func ParseInt(s string) (int, error) {
 	return strconv.Atoi(s)
-}
-
-// ParseIntOrPanic panics if ParseInt returns an error.
-func ParseIntOrPanic(s string) (int, error) {
-	r, err := ParseInt(s)
-	if err != nil {
-		panic(err)
-	}
-	return r, nil
 }
 
 // ParseUint returns strconv.ParseUint(s, 10, 0).
@@ -37,13 +19,4 @@ func ParseUint(s string) (uint, error) {
 		return 0, err
 	}
 	return uint(r), nil
-}
-
-// ParseUintOrPanic panics if ParseUint returns an error.
-func ParseUintOrPanic(s string) uint {
-	r, err := ParseUint(s)
-	if err != nil {
-		panic(err)
-	}
-	return r
 }
