9c572000a604e5f592484b5a0e9159b5d4128a89
Mgen <m@mgenware.com>
2016-05-25 00:53:35 +0800

    httpx: define mime types as const

--- httpx/mime.go
+++ httpx/mime.go
@@ -5,44 +5,25 @@ import (
 	"net/http"
 )
 
-// SetHTMLContentType sets response Content-Type header to "text/html".
-func SetHTMLContentType(w http.ResponseWriter) {
-	setContentType(w, "text/html")
+const (
+	MIMETypeText = "text/plain"
+	MIMETypeHTML = "text/html"
+	MIMETypePNG  = "image/png"
+	MIMETypeForm = "x-www-form-urlencoded"
+	MIMETypeJSON = "application/json"
+	MIMETypeRSS  = "application/rss+xml"
+	MIMETypeAtom = "application/atom+xml"
+)
+
+// SetResponseContentType sets response Content-Type header to a specified value.
+func SetResponseContentType(w http.ResponseWriter, contentType string) {
+	w.Header().Set("Content-Type", contentType)
 }
 
-// SetJSONContentType sets response Content-Type header to "application/json".
-func SetJSONContentType(w http.ResponseWriter) {
-	setContentType(w, "application/json")
-}
-
-// SetFormContentType sets response Content-Type header to "x-www-form-urlencoded".
-func SetFormContentType(w http.ResponseWriter) {
-	setContentType(w, "x-www-form-urlencoded")
-}
-
-// SetPNGContentType sets response Content-Type header to "image/png".
-func SetPNGContentType(w http.ResponseWriter) {
-	setContentType(w, "image/png")
-}
-
-// SetRSSContentType sets response Content-Type header to "application/rss+xml".
-func SetRSSContentType(w http.ResponseWriter) {
-	setContentType(w, "application/rss+xml")
-}
-
-// SetAtomContentType sets response Content-Type header to "application/atom+xml".
-func SetAtomContentType(w http.ResponseWriter) {
-	setContentType(w, "application/atom+xml")
-}
-
-// SetContentTypeFromExtension sets response Content-Type header from a user specified file extension.
-func SetContentTypeFromExtension(w http.ResponseWriter, extension string) {
+// SetResponseContentTypeFromExtension sets response Content-Type header from a file extension.
+func SetResponseContentTypeFromExtension(w http.ResponseWriter, extension string) {
 	mime := mime.TypeByExtension(extension)
 	if mime != "" {
 		setContentType(w, mime)
 	}
-}
-
-func setContentType(w http.ResponseWriter, contentType string) {
-	w.Header().Set("Content-Type", contentType)
 }
