fccbb36789ff56b948c169d954d95cb712154d45
Mgen <m@mgenware.com>
2016-06-09 11:51:15 +0800

    sqlx: Add error checking functions for GetLastInsertID and GetRowsAffected

---
+++ database/sqlx/errors.go
@@ -0,0 +1,61 @@
+package sqlx
+
+import (
+	"database/sql"
+)
+
+func GetLastInsertIDWithError(result sql.Result) (int64, error) {
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+func GetLastInsertIDUint64WithError(result sql.Result, err error) (uint64, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetLastInsertIDUint64(result)
+}
+
+func GetLastInsertIDIntWithError(result sql.Result, err error) (int, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetLastInsertIDInt(result)
+}
+
+func GetLastInsertIDUintWithError(result sql.Result, err error) (uint, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetLastInsertIDUint(result)
+}
+
+func GetRowsAffectedWithError(result sql.Result) (int64, error) {
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+func GetRowsAffectedUint64WithError(result sql.Result, err error) (uint64, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetRowsAffectedUint64(result)
+}
+
+func GetRowsAffectedIntWithError(result sql.Result, err error) (int, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetRowsAffectedInt(result)
+}
+
+func GetRowsAffectedUintWithError(result sql.Result, err error) (uint, error) {
+	if err != nil {
+		return 0, err
+	}
+	return GetRowsAffectedUint(result)
+}
