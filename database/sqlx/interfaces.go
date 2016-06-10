9a2fa155036a6d04adeb35a0e0dbf6abaee2c111
Mgen <m@mgenware.com>
2016-06-10 13:30:34 +0800

    sqlx: Add Scannable and Queryable interfaces

---
+++ database/sqlx/interfaces.go
@@ -0,0 +1,15 @@
+package sqlx
+
+import "database/sql"
+
+// Scannable interface defines a set of methods shared by sql.Row and sql.Rows
+type Scannable interface {
+	Scan(dest ...interface{}) error
+}
+
+// Queryable interface defines a set of methods shared by sql.DB and sql.Tx
+type Queryable interface {
+	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
+}
