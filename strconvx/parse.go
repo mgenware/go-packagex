80a39b30885869acbbbe247c2a790e327cdd56f0
Mgen <m@mgenware.com>
2018-05-14 14:51:01 +0800

    strconvx: add/update comments

--- strconvx/parse.go
+++ strconvx/parse.go
@@ -2,17 +2,17 @@ package strconvx
 
 import "strconv"
 
-// ParseFloat returns strconv.ParseFloat(s, 64).
+// ParseFloat calls strconv.ParseFloat(s, 64).
 func ParseFloat(s string) (float64, error) {
 	return strconv.ParseFloat(s, 64)
 }
 
-// ParseInt returns strconv.Atoi(s).
+// ParseInt calls strconv.Atoi(s).
 func ParseInt(s string) (int, error) {
 	return strconv.Atoi(s)
 }
 
-// ParseUint returns strconv.ParseUint(s, 10, 0).
+// ParseUint calls strconv.ParseUint(s, 10, 0).
 func ParseUint(s string) (uint, error) {
 	r, err := strconv.ParseUint(s, 10, 0)
 	if err != nil {
@@ -21,7 +21,7 @@ func ParseUint(s string) (uint, error) {
 	return uint(r), nil
 }
 
-// ParseInt64 returns strconv.ParseInt(s, 10, 64).
+// ParseInt64 calls strconv.ParseInt(s, 10, 64).
 func ParseInt64(s string) (int64, error) {
 	r, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
@@ -30,7 +30,7 @@ func ParseInt64(s string) (int64, error) {
 	return int64(r), nil
 }
 
-// ParseUint64 returns strconv.ParseUint(s, 10, 64).
+// ParseUint64 calls strconv.ParseUint(s, 10, 64).
 func ParseUint64(s string) (uint64, error) {
 	r, err := strconv.ParseUint(s, 10, 64)
 	if err != nil {
--- strconvx/toString.go
+++ strconvx/toString.go
@@ -2,18 +2,22 @@ package strconvx
 
 import "strconv"
 
+// ToStringInt calls strconv.FormatInt(int64(i), 10)
 func ToStringInt(i int) string {
 	return strconv.FormatInt(int64(i), 10)
 }
 
+// ToStringUint calls strconv.FormatUint(uint64(i), 10)
 func ToStringUint(i uint) string {
 	return strconv.FormatUint(uint64(i), 10)
 }
 
+// ToStringInt64 calls strconv.FormatInt(i, 10)
 func ToStringInt64(i int64) string {
 	return strconv.FormatInt(i, 10)
 }
 
+// ToStringUint64 calls strconv.FormatUint(i, 10)
 func ToStringUint64(i uint64) string {
 	return strconv.FormatUint(i, 10)
 }
