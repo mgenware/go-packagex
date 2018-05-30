ebcd54a13356b4ff202174efd2e906f33f42a9b9
Mgen <m@mgenware.com>
2018-05-30 14:53:50 +0800

    templatex: Add SetGlobalDevMode

--- templatex/view.go
+++ templatex/view.go
@@ -5,16 +5,28 @@ import (
 	"text/template"
 )
 
+var globalDevMode = false
+
+// SetGlobalDevMode sets the global dev mode variable (defaults to false). If true, view will be always reloaded before being executed, so that template file changes apply without needing to restart Go server.
+func SetGlobalDevMode(value bool) {
+	globalDevMode = value
+}
+
 // View wraps a Go text/template.Template object, providing ease of use.
 type View struct {
-	// template holds the internal Template object from "text/template".
+	// template holds the internal text/template Template object.
 	template *template.Template
 	file     string
 	devMode  bool
 }
 
 // MustParseView loads a view from the given file, and panics if parsing failed.
-func MustParseView(file string, devMode bool) *View {
+func MustParseView(file string) *View {
+	return MustParseViewWithDevMode(file, globalDevMode)
+}
+
+// MustParseViewWithDevMode behaves like MustParseView, but allows user to override the devMode.
+func MustParseViewWithDevMode(file string, devMode bool) *View {
 	t := MustParseFile(file)
 	return &View{template: t, file: file, devMode: devMode}
 }
@@ -24,7 +36,6 @@ func (view *View) Execute(wr io.Writer, data interface{}) error {
 	if view.devMode {
 		view.template = MustParseFile(view.file)
 	}
-
 	return view.template.Execute(wr, data)
 }
 
