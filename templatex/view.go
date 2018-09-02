20b92fd6d6a56d0c89a4d141206c6216e5e3aa29
Mgen <m@mgenware.com>
2018-09-02 18:58:10 +0800

    templatex: Remove globalDevMode

--- templatex/view.go
+++ templatex/view.go
@@ -6,13 +6,6 @@ import (
 	"text/template"
 )
 
-var globalDevMode = false
-
-// SetGlobalDevMode sets the global dev mode variable (defaults to false). If true, view will be always reloaded before being executed, so that template file changes apply without needing to restart Go server.
-func SetGlobalDevMode(value bool) {
-	globalDevMode = value
-}
-
 // View wraps a Go text/template.Template object, providing ease of use.
 type View struct {
 	// template holds the internal text/template Template object.
@@ -22,19 +15,14 @@ type View struct {
 }
 
 // MustParseView loads a view from the given file, and panics if parsing failed.
-func MustParseView(file string) *View {
-	return MustParseViewWithDevMode(file, globalDevMode)
+func MustParseView(file string, devMode bool) *View {
+	t := MustParseFile(file)
+	return &View{template: t, file: file, devMode: devMode}
 }
 
 // MustParseViewFromDirectory joins the dir and the file arguments and calls MustParseView.
-func MustParseViewFromDirectory(dir, file string) *View {
-	return MustParseView(filepath.Join(dir, file))
-}
-
-// MustParseViewWithDevMode behaves like MustParseView, but allows user to override the devMode.
-func MustParseViewWithDevMode(file string, devMode bool) *View {
-	t := MustParseFile(file)
-	return &View{template: t, file: file, devMode: devMode}
+func MustParseViewFromDirectory(dir, file string, devMode bool) *View {
+	return MustParseView(filepath.Join(dir, file), devMode)
 }
 
 // Execute applies this view to the given data object.
