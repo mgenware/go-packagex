b569aa6946faec2dca3930e47e2d7bb7d003e050
Mgen <m@mgenware.com>
2018-09-22 18:27:54 +0800

    sqlx: Add helpers for nullable types

---
+++ database/sqlx/nullables.go
@@ -0,0 +1,37 @@
+package sqlx
+
+import (
+	"database/sql"
+)
+
+func NewNullBool() sql.NullBool {
+	return sql.NullBool{}
+}
+
+func NewNullFloat64() sql.NullFloat64 {
+	return sql.NullFloat64{}
+}
+
+func NewNullInt64() sql.NullInt64 {
+	return sql.NullInt64{}
+}
+
+func NewNullString() sql.NullString {
+	return sql.NullString{}
+}
+
+func NewBool(value bool) sql.NullBool {
+	return sql.NullBool{Bool: value, Valid: true}
+}
+
+func NewFloat64(value float64) sql.NullFloat64 {
+	return sql.NullFloat64{Float64: value, Valid: true}
+}
+
+func NewInt64(value int64) sql.NullInt64 {
+	return sql.NullInt64{Int64: value, Valid: true}
+}
+
+func NewString(value string) sql.NullString {
+	return sql.NullString{String: value, Valid: true}
+}
