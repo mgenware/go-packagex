fea43aaf680bcdcd4bcb296952f2f5e24ad492ea
Mgen <m@mgenware.com>
2020-03-21 02:33:18 +0800

    Better handling of checking file or directory existence

--- iox/iox.go
+++ iox/iox.go
@@ -33,32 +33,32 @@ func PathExists(path string) (bool, error) {
 	return info != nil, nil
 }
 
-// FileExists checks if a file exists.
+// FileExists checks if the specified file exists.
 func FileExists(file string) (bool, error) {
-	info, err := os.Stat(file)
+	info, err := pathExistsCore(file)
 	if err != nil {
 		return false, err
 	}
-	return !info.IsDir(), nil
+	return info != nil && !info.IsDir(), nil
 }
 
-// IsFile returns true if path points to a file and false otherwise.
+// IsFile returns true if the specified path is a file.
 func IsFile(path string) bool {
 	fileExists, _ := FileExists(path)
 	return fileExists
 }
 
-// DirectoryExists checks if a directory exists.
+// DirectoryExists checks if the specified directory exists.
 func DirectoryExists(dir string) (bool, error) {
-	info, err := os.Stat(dir)
+	info, err := pathExistsCore(dir)
 	if err != nil {
 		return false, err
 	}
-	return info.IsDir(), nil
+	return info != nil && info.IsDir(), nil
 }
 
-// IsDirectory returns true if path points to a directory and false otherwise.
+// IsDirectory returns true if the specified path is a directory.
 func IsDirectory(path string) bool {
-	fileExists, _ := DirectoryExists(path)
-	return fileExists
+	dirExists, _ := DirectoryExists(path)
+	return dirExists
 }
