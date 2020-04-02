7f594adcfdea3c990745c65ded5f84626f286fb9
Mgen <m@mgenware.com>
2020-04-03 02:15:23 +0800

    Add test.Assert

--- test/test.go
+++ test/test.go
@@ -15,6 +15,11 @@ func Compare(t *testing.T, want, got interface{}) {
 	}
 }
 
+// Assert compares the given two values, and make testing fail if they are not equivalent.
+func Assert(t *testing.T, got, want interface{}) {
+	Compare(t, want, got)
+}
+
 // PanicIfErr panics if the given error is not nil.
 func PanicIfErr(err error) {
 	if err != nil {
